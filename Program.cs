e281b899fd5ec0a27cd5363a017c890334438322
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-10-20 12:47:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const double pi = 3.14;
+        double r = Convert.ToDouble(Console.ReadLine());
+        double p = pi * r*r;
+        Console.WriteLine(p);
+        
+    }
+}
