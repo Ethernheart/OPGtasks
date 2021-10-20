fac6c2f7d84f7034fedf5aee9522a2174d79b4a8
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-10-20 12:54:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)        
+        {
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if((a>=1) && (a<=3))
+            {
+                Console.WriteLine($"a={a}");
+                 
+            }
+            if ((b >= 1) && (b <= 3))
+            {
+                Console.WriteLine($"b={b}");
+
+            }
+            if ((c >= 1) && (c <= 3))
+            {
+                Console.WriteLine($"c={c}");
+
+            }
+            
+
+        }
+    }
+}
