1177c6a4370b3ba5d1748673e7147a1ec80640e9
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-10-20 13:37:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, m, j;
+            m = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=m;i++)
+            {
+                for(j=1;j<=i;j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
