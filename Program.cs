cb1d2c33d915d70b4a92305847b9f7b71fbc5a5d
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-10-20 12:55:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            a = Convert.ToDouble(Console.ReadLine());
+           
+
+            if (a >= 500 && a < 1000)
+            {
+                a = a - (a / 100 * 3);
+                
+                
+            }
+            if(a>=1000)
+            { 
+                a = a - (a / 100 * 5);
+            }
+            Console.WriteLine(a);
+
+
+
+
+
+        }
+    }
+}
