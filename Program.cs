be86111d4ae612438888e0df24623f8477c9ecc9
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-10-20 12:53:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = Convert.ToInt16(Console.ReadLine());
+            if (a == 1)
+            {
+                Console.WriteLine("Ponedel'nik");
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("Vtornik");
+            }
+            if (a == 3)
+            {
+                Console.WriteLine("Sreda");
+            }
+            if (a == 4)
+            {
+                Console.WriteLine("Chetverg");
+            }
+            if (a == 5)
+            {
+                Console.WriteLine("Piatniza");
+            }
+            if (a == 6)
+            {
+                Console.WriteLine("Subbota");
+            }
+            if (a == 7)
+            {
+                Console.WriteLine("Voskresen'e");
+            }
+        }
+    }
+}
