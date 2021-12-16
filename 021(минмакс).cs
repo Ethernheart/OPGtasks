9e4aebc78ac26665f9322ccf4ba1014ac7674989
Ethernheart <90245641+Ethernheart@users.noreply.github.com>
2021-12-16 16:08:31 +0300

    Update and rename 021(минимальное).cs to 021(минмакс).cs

--- 021(минмакс).cs
+++ 021(минмакс).cs
@@ -3,23 +3,31 @@ namespace ConsoleApp17
 {
     class Program
     {
-        static void Min(ref int min, int[] a)
+        static void Minmax(ref int min, int[] a, int max)
         {
             min = a[0];
+            max = a[0];
             for (int j = 0; j < a.Length; j++)
             {
                 if (min > a[j])
                     min = a[j];
             }
+            for (int j = 0; j < a.Length; j++)
+            {
+                if (max < a[j])
+                    max = a[j];
+            }
 
             Console.WriteLine(min);
+            Console.WriteLine(max);
         }
 
         static void Main(string[] args)
         {
             var a = Array.ConvertAll((Console.ReadLine().Split(' ')), s => int.Parse(s));
             int min = a[0];
-            Min(ref min, a);
+            int max = a[0];
+            Minmax(ref min, a, max);
         }
     }
 }
